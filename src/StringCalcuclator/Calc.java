c17f79e1644e3ea787fa9f24e17b7394d712ae66
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-21 12:16:44 +0200

    any numbers give their sum, task 2

--- src/StringCalcuclator/Calc.java
+++ src/StringCalcuclator/Calc.java
@@ -1,4 +1,22 @@
 package StringCalcuclator;
 
 public class Calc {
+
+    public static int Sum(String input) {
+        int sum;
+        String[] inputTab;
+        if(input.equals("")) {
+            return 0;
+        }
+        else {
+            inputTab = input.split(",");
+            sum = Integer.parseInt(inputTab[0]);
+            for(int i = 1; i < inputTab.length; i++){
+                sum+= Integer.parseInt(inputTab[i]);
+            }
+            return sum;
+        }
+    }
+
+
 }
--- src/Test/StringCalculatorTest.java
+++ src/Test/StringCalculatorTest.java
@@ -1,5 +1,41 @@
+package Test;
+
+import StringCalcuclator.Calc;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static StringCalcuclator.Calc.Sum;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
+
+    @Test
+    public void emptyInutReturnsZero() {
+        String input = "";
+        int result = Calc.Sum(input);
+        Assert.assertEquals(0, result);
+    }
+    @Ignore
+    @Test
+    public void oneNumberInInputReturnsNumber() {
+        String input = "1";
+        int result = Calc.Sum(input);
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void twoNumbersInInputReturnsTheirSum() {
+        String input = "100,2";
+        int result = Calc.Sum(input);
+        Assert.assertEquals(102, result);
+    }
+
+    @Test
+    public void AnyNumbersInInputReturnTheirSum() {
+        String input = "1,2,3,4";
+        int result = Calc.Sum(input);
+        Assert.assertEquals(10, result);
+    }
 
 }
