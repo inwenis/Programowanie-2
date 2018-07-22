50e9a358ec28f7623522c48502ba7020fdfd136d
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-22 10:14:41 +0200

    listing numbers in a given range telling Fizz instead of 3 and x*3 and Bazz instead of 5 and x*5

--- src/FizzBazz/FizzBazz.java
+++ src/FizzBazz/FizzBazz.java
@@ -2,28 +2,23 @@ package FizzBazz;
 
 public class FizzBazz {
 
-    private static StringBuilder countingResult = new StringBuilder();
-
     public static String countFizzBuzz(int input1, int input2) {
+        StringBuilder countingResult = new StringBuilder();
 
         for (int i = input1; i <= input2; i++) {
             if (i % 3 == 0) {
                 countingResult.append("Fizz ");
             }
+            else if (i % 5 == 0) {
+                countingResult.append("Bazz ");
+            }
             else {
                 countingResult.append(i + " ");
             }
         }
-        String countingResultString = countingResult.toString();
-        return countingResultString;
+        return countingResult.toString();
     }
 
 
 }
-/*
-1. podajesz dwie liczby
-2. jeżeli jest podzielina przez 3, Fizz
-3. jeżeli jest podzielna  przez 5, Buzz
-4. jeżeli jest  podzielna  przez 3 i 5 - FizzBazz
 
- */
--- src/Test/FizzBazzTest.java
+++ src/Test/FizzBazzTest.java
@@ -11,8 +11,16 @@ public class FizzBazzTest {
     @Test
     public void choosingThrees() {
         int input1 = 1;
-        int input2 = 9;
+        int input2 = 4;
         String result = FizzBazz.countFizzBuzz(input1, input2);
-        Assert.assertEquals("1 2 Fizz 4 5 Fizz 7 8 Fizz ", result);
+        Assert.assertEquals("1 2 Fizz 4 ", result);
+    }
+
+    @Test
+    public void choosingThreesAndFives() {
+        int input1 = 1;
+        int input2 = 10;
+        String result = FizzBazz.countFizzBuzz(input1, input2);
+        Assert.assertEquals("1 2 Fizz 4 Bazz Fizz 7 8 Fizz Bazz ", result);
     }
 }
